e1b9800ab62b3b3bb33b5f9004b08e2786ce1e2e
Zeshan Haider <mhaider.zeshan@gmail.com>
2026-04-30 03:59:02 +0500

    Changed the DB URL

--- app/db.py
+++ app/db.py
@@ -3,19 +3,10 @@ from sqlalchemy.orm import DeclarativeBase
 
 from app.core.config import settings
 
-DB_USER = settings.DB_USER
-DB_PASSWORD = settings.DB_PASSWORD.get_secret_value()
-DB_HOST = settings.DB_HOST
-DB_PORT = settings.DB_PORT
-DB_NAME = settings.DB_NAME
+if not settings.DATABASE_URL:
+    raise ValueError("DATABASE_URL not found in environment variables.")
 
-DATABASE_URL = (
-    f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-)
-if not DATABASE_URL:
-    raise ValueError("Database URL not found in environment variables.")
-
-engine = create_async_engine(DATABASE_URL, echo=True)
+engine = create_async_engine(settings.DATABASE_URL, echo=True)
 
 SessionLocal = async_sessionmaker(
     bind=engine, class_=AsyncSession, expire_on_commit=False
