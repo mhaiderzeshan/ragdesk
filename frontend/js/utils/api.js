c404e2a9ccfe70df91dcaf5208ab9280a352728a
Zeshan Haider <mhaider.zeshan@gmail.com>
2026-05-06 00:57:21 +0500

    add the live backend URL to frontend

--- frontend/js/utils/api.js
+++ frontend/js/utils/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = typeof CONFIG !== 'undefined' ? CONFIG.API_URL : 'http://localhost:8000';
+const BASE_URL = typeof CONFIG !== 'undefined' ? CONFIG.API_URL : 'https://api-service-production-46be.up.railway.app';
 
 export const API = {
   getToken() {
